95e68bb74248001594dd67c373da40cceb4c0661
Albert Viaplana <pichi.pride@gmail.com>
2020-05-23 19:49:46 +0200

    remove strcat

--- lib/TimerRepository/src/DataSource/LocalDS.cpp
+++ lib/TimerRepository/src/DataSource/LocalDS.cpp
@@ -12,11 +12,10 @@ Project** LocalDS::getProjects() {
     struct Project** projects = (struct Project**) malloc(sizeof(struct Project*) * num_projects);
 
     for (int i = 0; i < num_projects; i++) {
-        char project_num = i + '0';
         projects[i] = (struct Project*) malloc(sizeof(struct Project));
 
-        strcpy(projects[i]->id, strcat("FakeId", &project_num));
-        strcpy(projects[i]->name, strcat("Project name ", &project_num));
+        strcpy(projects[i]->id, "FakeId");
+        strcpy(projects[i]->name, "Project name ");
         strcpy(projects[i]->color, "#AABBCC");
     }
 
